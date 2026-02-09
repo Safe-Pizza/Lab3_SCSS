30807990109bd99ff1706aa934194e83004a3bbc
Hanna Lindkvist <hali2507@student.miun.se>
2026-02-09 09:32:33 +0100

    styling för mobil navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+document.querySelector("#hamburger-menu").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    var x = document.querySelector("#main-nav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
