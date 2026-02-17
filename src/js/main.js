22f88ff722d17a4c7cd07ca84f376261d288da19
Hanna Lindkvist <hali2507@student.miun.se>
2026-02-17 15:45:27 +0100

    skapar animation för laddning

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 //Toggle funktion för mobil meny
 document.querySelector("#hamburger-menu").addEventListener("click", toggleMenu);
+document.querySelector(".toggle").addEventListener("click", toggleDiv);
 
 function toggleMenu() {
     const mainNavEl = document.querySelector("#main-nav");
@@ -11,5 +12,15 @@ function toggleMenu() {
     } else {
         mainNavEl.style.display = "block";
         hamMenuEl.classList.add("change");
+    }
+}
+
+function toggleDiv() {
+    const divEl = document.querySelector(".container-loader");
+
+    if (divEl.style.display === "grid") {
+        divEl.style.display = "none";
+    } else {
+        divEl.style.display = "grid";
     }
 }
