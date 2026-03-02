565872dca3f9300f7992c1d2a60a8cde4119f5f7
Hanna Lindkvist <hali2507@student.miun.se>
2026-03-02 09:56:51 +0100

    justerar config för svg

--- vite.config.js
+++ vite.config.js
@@ -37,6 +37,32 @@ export default defineConfig({
             },
             avif: {
                 quality: 60
+            },
+            svg: {
+                multipass: true,
+                plugins: [
+                    {
+                        name: 'preset-default',
+                        params: {
+                            overrides: {
+                                cleanupNumericValues: false,
+                                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                            },
+                            cleanupIDs: {
+                                minify: false,
+                                remove: false,
+                            },
+                            convertPathData: false,
+                        },
+                    },
+                    'sortAttrs',
+                    {
+                        name: 'addAttributesToSVGElement',
+                        params: {
+                            attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                        },
+                    },
+                ]
             }
         })
     ]
